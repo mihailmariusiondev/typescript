5c757992e2e2c060e4a002808eccff49115d69ad
MariusMihailEnzo <marius.mihail@enzo.es>
2021-09-09 12:40:05 +0200

    06-desestructuracion-funcion

---
+++ src/Ejercicios/06-desestructuracion-funcion.ts
@@ -0,0 +1,34 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Producto {
+    desc: string;
+    precio: number;
+}
+
+const telefono: Producto = {
+    desc: 'Nokia A1',
+    precio: 150
+}
+
+const tableta: Producto = {
+    desc: 'iPad Air',
+    precio: 350
+}
+
+function calcularISV(productos: Producto[]) : [number, number]{
+    let total = 0;
+
+    productos.forEach(({precio}) => {
+        total += precio;
+    })
+
+    return [total, total * 0.15];
+}
+
+const articulos = [telefono, tableta]
+const [total, isv] = calcularISV(articulos);
+
+console.log(`total ${total}`);
+console.log(`isv ${isv}`);
