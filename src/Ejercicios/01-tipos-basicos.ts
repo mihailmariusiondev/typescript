c56f9b8c5127ecedcd526c6920985a7348675628
MariusMihailEnzo <marius.mihail@enzo.es>
2021-09-09 11:17:02 +0200

    01-tipos-basicos

---
+++ src/Ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+let nombre: string = 'Strider';
+let hp: number | string = 95; //nombre o string
+let estaVivo: boolean = true;
+
+hp = 'FULL';
+
+console.log(nombre, hp, estaVivo);
